e12695913bc0f04c31f5f201a501a4ebe7a5c728
r6cky <cleitonrocha321@hotmail.com>
2023-11-09 10:50:59 -0300

    create activation of modal edit department

--- ReactJS-FrontEnd/src/components/Modals/DepartmentEdit/index.tsx
+++ ReactJS-FrontEnd/src/components/Modals/DepartmentEdit/index.tsx
@@ -1,9 +1,24 @@
+import { useContext } from "react";
+import { ModalContext } from "../../../context/ContextModals";
 import { StyleDepartmentEdit } from "./style";
 
 export const DepartmentEdit = () => {
+  const { activateModal, setModalDepartmentEdit, modalDepartmentEdit } =
+    useContext(ModalContext);
+
   return (
     <StyleDepartmentEdit>
       <div className="form-area">
+        <div className="close-modal">
+          <span
+            className="btn-close-modal"
+            onClick={() => {
+              return activateModal(modalDepartmentEdit, setModalDepartmentEdit);
+            }}
+          >
+            X
+          </span>
+        </div>
         <h2 className="title-department-edit">Editar departamento</h2>
         <form
           className="form-department-create"
--- ReactJS-FrontEnd/src/components/Modals/DepartmentEdit/style.ts
+++ ReactJS-FrontEnd/src/components/Modals/DepartmentEdit/style.ts
@@ -3,13 +3,36 @@ import styled from "styled-components";
 export const StyleDepartmentEdit = styled.div`
 
     border:  solid red;
-
     width: 100%;
     max-width: 100%;
-    height: 100%;
+    height: 100vh;
     display: flex;
     justify-content: center;
     align-content: center;
+    position: fixed;
+    top: 0px;
+    background: rgba(0, 0, 0, 0.8);
+  
+
+    .close-modal{
+        border: solid;
+        width: 100%;
+        height: 40px;
+        display: flex;
+        justify-content: flex-end;
+
+        span{
+            border-radius: 50%;
+            border: solid ;
+            width: 40px;
+            height: 40px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: white;
+            cursor: pointer;
+        }
+    }
 
     .form-area{
         border: solid red;
