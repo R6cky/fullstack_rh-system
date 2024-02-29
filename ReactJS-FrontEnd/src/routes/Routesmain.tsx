c132736340caef9670d393d0c561b1d329ef6d11
r6cky <cleitonrocha321@hotmail.com>
2024-02-29 12:53:23 -0300

    feat: add toastify in login feature

--- ReactJS-FrontEnd/src/routes/Routesmain.tsx
+++ ReactJS-FrontEnd/src/routes/Routesmain.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "../components/pages/Home";
 import { Login } from "../components/pages/Login";
 import { Register } from "../components/pages/Register";
