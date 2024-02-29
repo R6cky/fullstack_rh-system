f27f1d630fa277bbf1dfc0976a774b8c53329d5c
r6cky <cleitonrocha321@hotmail.com>
2024-02-29 12:48:59 -0300

    feat: add toastify in modal department edit

--- ReactJS-FrontEnd/src/components/pages/MainPageAdmin/index.tsx
+++ ReactJS-FrontEnd/src/components/pages/MainPageAdmin/index.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Footer } from "../../Footer";
 import { DepartmentList } from "./DepartmentList";
 import { RegisteredUserlist } from "./RegisteredUserList";
